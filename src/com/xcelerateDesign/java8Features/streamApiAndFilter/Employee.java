dd4c63151e64e939742d7b8276d7d464582373da
to-jinalmodi <to.jinalmodi@gmail.com>
2023-12-02 09:59:12 -0500

    Stream api

---
+++ src/com/xcelerateDesign/java8Features/streamApiAndFilter/Employee.java
@@ -0,0 +1,36 @@
+package com.xcelerateDesign.java8Features.streamApiAndFilter;
+
+/**
+ * Employee class.
+ */
+public class Employee {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String country;
+
+    public Employee(Integer id, String firstName, String lastName, String country) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+}
